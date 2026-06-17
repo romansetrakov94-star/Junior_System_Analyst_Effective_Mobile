bca465706dba6ca24e7243876e9dc6c160048ee7
romansetrakov94-star <roman.setrakov94@gmail.com>
2026-06-17 11:07:02 +0300

    Update task3_purchases.sql

--- sql/task3_purchases.sql
+++ sql/task3_purchases.sql
@@ -1,12 +1,13 @@
-<<<<<<< HEAD
+-- Задание 3: Покупки (без подзапросов и оконных функций)
+-- Найти ID клиентов, у которых сумма покупок за последний календарный месяц меньше 5000
+
 SELECT 
     a.client_id
 FROM account a
 JOIN transaction t ON a.id = t.account_id
-WHERE t.transaction_date >= CURRENT_DATE - INTERVAL '1 month'
+WHERE t.transaction_date >= DATE_TRUNC('month', CURRENT_DATE - INTERVAL '1 month')
+  AND t.transaction_date < DATE_TRUNC('month', CURRENT_DATE)
   AND t.type = 'purchase'
+  AND a.close_dt IS NULL   -- счёт активен
 GROUP BY a.client_id
 HAVING SUM(t.amount) < 5000;
-=======
-sql<br>-- Задание 3: Покупки (без подзапросов и оконных функций)<br>-- Найти ID клиентов, у которых сумма покупок за последний месяц меньше 5000<br><br>SELECT <br> a.client_id<br>FROM account a<br>JOIN transaction t ON a.id = t.account_id<br>WHERE t.transaction_date >= CURRENT_DATE - INTERVAL '1 month'<br> AND t.type = 'purchase'<br>GROUP BY a.client_id<br>HAVING SUM(t.amount) < 5000;<br>
->>>>>>> 6072b97515edcb726b5ff070f5e8b80f13f462a2
